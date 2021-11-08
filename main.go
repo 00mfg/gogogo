87c0dc771584427e796833f56bff536e88a5cdc5
lc <tron.liu@gmail.com>
2021-11-08 17:25:11 +0800

    v3

--- main.go
+++ main.go
@@ -12,15 +12,21 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 660)
+	return ioutil.WriteFile(filename, p.Body, 0660)
 }
 
-func loadpage(title string) {
-
+func loadpage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
-func editHandle() {
 
-}
 func main() {
-	fmt.Println("hello, world")
+	p1 := &Page{Title: "test", Body: []byte("test page")}
+	p1.save()
+	p2, _ := loadpage("test")
+	fmt.Println(string(p2.Body))
 }
